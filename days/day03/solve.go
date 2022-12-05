a27442e3b42922863e1d304e52c16b5b67a46650
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-12-04 22:23:09 -0500

    Save solution for day 3, part 2.

--- days/day03/solve.go
+++ days/day03/solve.go
@@ -31,12 +31,40 @@ func (c Compartment) String() string {
 	return strings.Join(out, "")
 }
 
+func FirstCommonItem(compartments ...Compartment) *Item {
+	common := make(Compartment)
+
+	for _, current := range compartments {
+		for item := range current {
+			common[item] += 1
+		}
+	}
+
+	for item, count := range common {
+		if count == len(compartments) {
+			return &item
+		}
+	}
+	return nil
+}
+
 type Rucksack struct {
 	First, Second Compartment
 }
 
 func (s *Rucksack) String() string {
 	return s.First.String() + " | " + s.Second.String()
+}
+
+func (s *Rucksack) CombinedContents() Compartment {
+	c := make(Compartment)
+	for item, count := range s.First {
+		c[item] = count
+	}
+	for item, count := range s.Second {
+		c[item] += count
+	}
+	return c
 }
 
 func SolvePuzzle(input aoc.Input) (s aoc.Solution, err error) {
@@ -68,16 +96,41 @@ func SolvePuzzle(input aoc.Input) (s aoc.Solution, err error) {
 
 	var totalPriority int
 
-	for _, sack := range sacks {
-		for item := range sack.First {
-			if _, ok := sack.Second[item]; ok {
-				totalPriority += item.Priority()
-				break
-			}
+	for index, sack := range sacks {
+		itemPtr := FirstCommonItem(sack.First, sack.Second)
+		if itemPtr == nil {
+			err = fmt.Errorf("no common item for sack on line %d", index+1)
+			return
 		}
+
+		totalPriority += itemPtr.Priority()
 	}
 
 	s.Part1.SaveAnswer(totalPriority)
+
+	if len(sacks)%3 != 0 {
+		err = fmt.Errorf("sack count of %d not divisible by 3", len(sacks))
+		return
+	}
+
+	totalPriority = 0
+
+	for i := 0; i < len(sacks); i += 3 {
+		c1 := sacks[i].CombinedContents()
+		c2 := sacks[i+1].CombinedContents()
+		c3 := sacks[i+2].CombinedContents()
+
+		itemPtr := FirstCommonItem(c1, c2, c3)
+		if itemPtr == nil {
+			err = fmt.Errorf("no common item for group starting at line %d",
+				(i*3)+1)
+			return
+		}
+
+		totalPriority += itemPtr.Priority()
+	}
+
+	s.Part2.SaveAnswer(totalPriority)
 
 	return
 }
