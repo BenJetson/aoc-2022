ad035c34db8c0d87b5e95bbbc5d392c503dd1072
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-12-04 19:07:50 -0500

    Initialize day 2!

--- days/days.go
+++ days/days.go
@@ -4,11 +4,13 @@ import (
 	"github.com/BenJetson/aoc-2022/aoc"
 	// BEGIN DAY IMPORTS
 	"github.com/BenJetson/aoc-2022/days/day01"
+	"github.com/BenJetson/aoc-2022/days/day02"
 	// END DAY IMPORTS
 )
 
 var Solvers = map[int]aoc.Solver{
 	// BEGIN DAY SOLVERS
 	1: day01.SolvePuzzle,
+	2: day02.SolvePuzzle,
 	// END DAY SOLVERS
 }
