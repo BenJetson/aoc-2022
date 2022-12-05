3b421a82111d532dcef5b38d308e84bec8ccfc77
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-12-04 19:03:17 -0500

    Save solution for day 1, part 1.

--- days/day01/solve.go
+++ days/day01/solve.go
@@ -1,7 +1,62 @@
 package day01
 
-import "github.com/BenJetson/aoc-2022/aoc"
+import (
+	"errors"
+	"fmt"
+	"sort"
+	"strconv"
+
+	"github.com/BenJetson/aoc-2022/aoc"
+)
+
+type Food struct {
+	Calories int
+}
+
+type Elf struct {
+	Inventory []Food
+}
+
+func (e *Elf) TotalCaloriesCarried() (total int) {
+	for _, f := range e.Inventory {
+		total += f.Calories
+	}
+	return
+}
 
 func SolvePuzzle(input aoc.Input) (s aoc.Solution, err error) {
+	var elves []Elf
+
+	var current Elf
+	for index, line := range input {
+		if line == "" {
+			elves = append(elves, current)
+			current = Elf{}
+			continue
+		}
+
+		var calories int
+
+		if calories, err = strconv.Atoi(line); err != nil {
+			err = fmt.Errorf("cannot convert calories on line %d: %w",
+				index+1, err)
+			return
+		}
+
+		current.Inventory = append(current.Inventory, Food{Calories: calories})
+	}
+
+	if len(elves) < 1 {
+		err = errors.New("no elves found")
+		return
+	}
+
+	// Sort so the elf with the most calories will be at index zero.
+	sort.Slice(elves, func(i, j int) bool {
+		return elves[i].TotalCaloriesCarried() > elves[j].TotalCaloriesCarried()
+	})
+
+	s.Part1.SaveAnswer(elves[0].TotalCaloriesCarried())
+
 	return
 }
