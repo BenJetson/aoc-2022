db9c328a674bc093e58a1052ffa6e6ab060a0046
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-12-09 23:55:31 -0500

    Separate day tests into 2 sub-tests; skip if no known solution.

--- solver/solve_test.go
+++ solver/solve_test.go
@@ -25,32 +25,40 @@ func TestSolvers(t *testing.T) {
 		t.Run(dayStr, func(t *testing.T) {
 			knownSolution, err := aoc.GetSolution(day)
 			require.NoError(t, err, "require solution to check")
-			require.True(t, knownSolution.Part1.Valid,
-				"known solution part 1 answer must be valid to check")
-			require.True(t, knownSolution.Part2.Valid,
-				"known solution part 2 answer must be valid to check")
 
 			solution, err := RunForDay(day)
 			require.NoError(t, err, "expect no error when solving")
 
-			assert.True(t, solution.Part1.Valid,
-				"part 1 answer ought to be valid")
-			if solution.Part1.Valid {
-				assert.Equal(t,
-					knownSolution.Part1.Value,
-					solution.Part1.Value,
-					"part 1 answer ought to match known answer",
-				)
-			}
-			assert.True(t, solution.Part2.Valid,
-				"part 2 answer ought to be valid")
-			if solution.Part2.Valid {
-				assert.Equal(t,
-					knownSolution.Part2.Value,
-					solution.Part2.Value,
-					"part 2 answer ought to match known answer",
-				)
-			}
+			t.Run("part1", func(t *testing.T) {
+				if !knownSolution.Part1.Valid {
+					t.SkipNow()
+				}
+
+				assert.True(t, solution.Part1.Valid,
+					"part 1 answer ought to be valid")
+				if solution.Part1.Valid {
+					assert.Equal(t,
+						knownSolution.Part1.Value,
+						solution.Part1.Value,
+						"part 1 answer ought to match known answer",
+					)
+				}
+			})
+			t.Run("part2", func(t *testing.T) {
+				if !knownSolution.Part2.Valid {
+					t.SkipNow()
+				}
+
+				assert.True(t, solution.Part2.Valid,
+					"part 2 answer ought to be valid")
+				if solution.Part2.Valid {
+					assert.Equal(t,
+						knownSolution.Part2.Value,
+						solution.Part2.Value,
+						"part 2 answer ought to match known answer",
+					)
+				}
+			})
 		})
 	}
 }
