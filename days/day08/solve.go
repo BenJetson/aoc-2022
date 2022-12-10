d3be164579f0a8a2c255d38b3684d53148e96a43
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-12-09 23:48:24 -0500

    Save code and answer for day 8, part 1.

--- days/day08/solve.go
+++ days/day08/solve.go
@@ -1,7 +1,179 @@
 package day08
 
-import "github.com/BenJetson/aoc-2022/aoc"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/BenJetson/aoc-2022/aoc"
+)
+
+type Tree struct {
+	Height int
+	Seen   bool
+}
+
+type ForestRow []Tree
+
+func (fr ForestRow) Col(c int) *Tree {
+	if fr == nil || c < 0 || c >= len(fr) {
+		return nil
+	}
+	return &fr[c]
+}
+
+// Forest represents a grid of trees. Coordinates are like the PyGame grid
+// system, where (0,0) is the upper left and y values increase as you go down.
+type Forest []ForestRow
+
+func (f Forest) Row(r int) ForestRow {
+	if f == nil || r < 0 || r >= len(f) {
+		return nil
+	}
+	return f[r]
+}
+
+func (f Forest) RowCount() int {
+	if len(f) == 0 {
+		return 0
+	}
+	return len(f[0])
+}
+
+func (f Forest) ColCount() int {
+	return len(f)
+}
+
+func (f Forest) String() string {
+	var s strings.Builder
+	for r := 0; r < f.RowCount(); r++ {
+		for c := 0; c < f.ColCount(); c++ {
+			t := f.Row(r).Col(c)
+			s.WriteString(strconv.Itoa(t.Height))
+			if t.Seen {
+				s.WriteString("s")
+			} else {
+				s.WriteString("_")
+			}
+			s.WriteString(" ")
+		}
+		s.WriteString("\n")
+	}
+	return s.String()
+}
+
+func ReadForestFromLines(lines aoc.Input) (f Forest, err error) {
+	f = make(Forest, len(lines))
+	for r, line := range lines {
+		f[r] = make(ForestRow, len(line))
+	}
+
+	for r, line := range lines {
+		for c, char := range line {
+			t := f.Row(r).Col(c)
+			t.Height, err = strconv.Atoi(string(char))
+			if err != nil {
+				err = fmt.Errorf("bad height for tree at pos (%d, %d): %w",
+					r, c, err)
+				return
+			}
+		}
+	}
+
+	return
+}
+
+func Max(x, y int) int {
+	if y > x {
+		return y
+	}
+	return x
+}
+
+type Position struct {
+	r, c int
+}
+
+func (p *Position) Move(v Vector) {
+	p.r += v.r
+	p.c += v.c
+}
+
+type Vector struct {
+	r, c int
+}
+
+func (f Forest) Scan(start Position, v Vector) {
+	p := start
+	t := f.Row(p.r).Col(p.c)
+	highestSoFar := t.Height
+	p.Move(v)
+	t = f.Row(p.r).Col(p.c)
+
+	for t != nil {
+		isTallerThanHighest := t.Height > highestSoFar
+		if isTallerThanHighest {
+			t.Seen = true
+			highestSoFar = t.Height
+		}
+
+		p.Move(v)
+		t = f.Row(p.r).Col(p.c)
+	}
+}
+
+func (f Forest) MarkVisibleTrees() {
+	rowCount := f.RowCount()
+	colCount := f.ColCount()
+
+	for r := 0; r < rowCount; r++ {
+		row := f.Row(r)
+
+		// Leftmost and rightmost tree are always perimiter.
+		row.Col(0).Seen = true
+		row.Col(colCount - 1).Seen = true
+
+		// On first and last rows, all trees from 1 to colCount-1 are perimiter.
+		if r == 0 || r == rowCount-1 {
+			for c := 1; c < colCount-1; c++ {
+				row.Col(c).Seen = true
+			}
+		}
+	}
+
+	for r := 1; r < rowCount-1; r++ {
+		f.Scan(Position{r: r, c: 0}, Vector{r: 0, c: 1})
+		f.Scan(Position{r: r, c: colCount - 1}, Vector{r: 0, c: -1})
+	}
+
+	for c := 1; c < colCount-1; c++ {
+		f.Scan(Position{r: 0, c: c}, Vector{r: 1, c: 0})
+		f.Scan(Position{r: rowCount - 1, c: c}, Vector{r: -1, c: 0})
+	}
+
+}
+
+func (f Forest) CountSeenTrees() (count int) {
+	for r := 0; r < f.RowCount(); r++ {
+		for c := 0; c < f.ColCount(); c++ {
+			if f.Row(r).Col(c).Seen {
+				count++
+			}
+		}
+	}
+	return
+}
 
 func SolvePuzzle(input aoc.Input) (s aoc.Solution, err error) {
+	f, err := ReadForestFromLines(input)
+	if err != nil {
+		err = fmt.Errorf("could not read forest: %w", err)
+		return
+	}
+
+	f.MarkVisibleTrees()
+	count := f.CountSeenTrees()
+	s.Part1.SaveIntAnswer(count)
+
 	return
 }
